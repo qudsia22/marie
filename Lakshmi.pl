fbdd138fe2f033500e61dbdc8d18fa846223efa6
Lakshmip108 <88369455+Lakshmip108@users.noreply.github.com>
2021-08-06 21:14:25 +0530

    Update Lakshmi.pl

--- Lakshmi.pl
+++ Lakshmi.pl
@@ -1,20 +1,2 @@
 #!/usr/bin/perl
-$name='Lakshmi S Prasad';
-print "Name $name \n";
-$email='lakshmiprasad018@gmail.com';
-print "email_id $email \n";
-$slack_username= '@lakshmip';
-print "slack username: $slack_username \n";
-print "biostack: Functional_Genomics\n";
-$twitter_handle= '@Lakshmip2798';
-print "twitter_handle: $twitter_handle \n";
-print "$hd";
-$hdf=hd($slack_username,$twitter_handle);
-print "hamming_distance: $hdf";
-
-sub hd{
-	($k, $l) = @_;
-	$diff = $k ^ $l;
-	$num_mismatch = $diff =~ tr/\0//c;
-	return $num_mismatch;
-}
+$name='Lakshmi S Prasad';$email='lakshmiprasad018@gmail.com';$slack_username= '@lakshmip';$twitter_handle='@Lakshmip2798';print "$hd";$hdf=hd($slack_username,$twitter_handle);print "$name,$email$slack_username,Functional_Genomics,$twitter_handle,$hdf";sub hd{($k, $l) = @_;$diff =$k ^ $l;$num_mismatch = $diff =~ tr/\0//c;return$num_mismatch;}
