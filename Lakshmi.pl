c3a92588638823cbc186053edb5905f499cd9e01
Shruti <44881300+imshruti@users.noreply.github.com>
2021-08-07 00:32:57 +0530

    Update Lakshmi.pl

--- Lakshmi.pl
+++ Lakshmi.pl
@@ -1,2 +1,2 @@
 #!/usr/bin/perl
-$name='Lakshmi S Prasad';$email='lakshmiprasad018@gmail.com';$slack_username= '@lakshmip';$twitter_handle='@Lakshmip2798';print "$hd";$hdf=hd($slack_username,$twitter_handle);print "$name,$email$slack_username,Functional_Genomics,$twitter_handle,$hdf";sub hd{($k, $l) = @_;$diff =$k ^ $l;$num_mismatch = $diff =~ tr/\0//c;return$num_mismatch;}
+$name='Lakshmi S Prasad';$email='lakshmiprasad018@gmail.com';$slack_username= '@lakshmip';$twitter_handle='@Lakshmip2798';print "$hd";$hdf=hd($slack_username,$twitter_handle);print "$name,$email,$slack_username,Functional_Genomics,$twitter_handle,$hdf";sub hd{($k, $l) = @_;$diff =$k ^ $l;$num_mismatch = $diff =~ tr/\0//c;return$num_mismatch;}
